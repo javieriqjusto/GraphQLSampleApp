1360740739c8278f893af7d93810e530a26ed6fc
javieriqjusto <javier.iniguez@justo.mx>
2023-01-11 11:35:09 -0600

    Instance of Apollo Client created

--- app/src/main/java/com/example/graphqlsample/GraphqlSampleApp.kt
+++ app/src/main/java/com/example/graphqlsample/GraphqlSampleApp.kt
@@ -1,4 +1,7 @@
 package com.example.graphqlsample
 
-class GraphqlSampleApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GraphqlSampleApp:Application()
